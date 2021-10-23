e4bb9682e4a5674795276b90c83ce1f2e17cd484
Ian <yan.dolganskiy@mail.ru>
2021-10-23 16:40:43 +0300

    Replace datasource to a local test database

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -16,7 +16,7 @@ import java.sql.Statement;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+        try (Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/products")) {
             String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
                     "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
                     " NAME           TEXT    NOT NULL, " +
